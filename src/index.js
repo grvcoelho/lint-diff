101f264d07b5cc27ba329ea2218e8d371ce1347d
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2018-05-01 08:32:35 -0300

    command: remove redundant lint command

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import program from 'commander'
+import meow from 'meow'
 import run from './lint-diff'
 
-program
-  .command('lint <commit-range>')
-  .action(run)
+const debug = () => {
+  console.log('debug')
+}
 
-program.parse(process.argv)
+const cli = meow(`
+  Usage
+    $ lint-diff [<diff-input>]
+
+  Examples
+    $ lint-diff
+    $ lint-diff HEAD~1..HEAD
+    $ lint-diff master..my-branch
+`)
+
+run(cli.input[0])
